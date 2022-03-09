1d497b2e6dd14d93740ddc112d926dbe61033461
Jeremy Elson <jelson@gmail.com>
2022-03-09 08:49:12 -0800

    various improvements to the cnt-90 logger
    * look for the cnt-90 specifically rather than any usb instrument
    
    * name files more semantically, using argv-provided prefix and timestamp
    
    * add newline explicitly to each line rather than assuming the
      response from the instrument has a newline in it

--- loggers/cnt90.py
+++ loggers/cnt90.py
@@ -4,26 +4,37 @@ import pyvisa
 import sys
 import time
 
+# VISA instrument id for CNT-90
+INSTR_ID = "5355::144"
+
 def get_instr():
     rm = pyvisa.ResourceManager()
     instrs = rm.list_resources()
     print(f"All instruments: {instrs}")
-    usb_instrs = [i for i in instrs if 'USB' in i]
-    print(f"USB instruments: {usb_instrs}")
-    if len(usb_instrs) != 1:
-        print("can't find right instrument")
+    matches = [i for i in instrs if INSTR_ID in i]
+    if len(matches) != 1:
+        print(f"can't find right instrument with ID {INSTR_ID}")
         sys.exit(1)
-    instr = rm.open_resource(usb_instrs[0])
+    instr_name = matches[0]
+    print(f"Instrument found: {instr_name}")
+    instr = rm.open_resource(instr_name)
     del instr.timeout
     print(instr.query("*IDN?"))
     return instr
 
 def main():
+    if not len(sys.argv) == 2:
+        print(f"Usage: {sys.argv[0]} logfile-prefix")
+        sys.exit(1)
+
+    fn = f"{sys.argv[1]}_{round(time.time())}.csv"
+    print(f"Writing to {fn}")
+
     instr = get_instr()
-    with open("data.txt", "a") as outfile:
+    with open(fn, "a") as outfile:
         while True:
-            val = instr.query("MEAS:FREQ?")
-            out = f"{time.time()},{val}"
+            val = instr.query("MEAS:FREQ?").strip()
+            out = f"{time.time():.6f},{val}\n"
             sys.stdout.write(out)
             outfile.write(out)
 
