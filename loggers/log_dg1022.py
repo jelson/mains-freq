908342e0b5d76efda83890ef52db4175d13869b7
Dave Jacobowitz <david.jacobowitz@gmail.com>
2022-03-09 17:49:49 -0800

    d2

--- loggers/log_dg1022.py
+++ loggers/log_dg1022.py
@@ -28,24 +28,30 @@ def myW(m):
 
 print(myQ("*IDN?"))
 
-#myW('FUNC RAMP')
-#myW('APPLY:SIN 1000,1,1')
-myW('OUTP OFF')
-myW('OUTP:CH2 OFF')
-myW('SYST:CLKSRC EXT')
-myW('COUN ON')
-myW('COUN:COUP DC')
-print(myQ('COUN:COUP?'))
-myW('COUN:SENS LOW')
-print(myQ('COUN:SENS?'))
-myW('COUN:TLEV 50')
-print(myQ('COUN:TLEV?'))
-myW('COUN:HFRS ON')
-print(myQ('COUN:HFRS?'))
-print(myQ('COUN:FREQ?'))
+if False:
+    #myW('FUNC RAMP')
+    #myW('APPLY:SIN 1000,1,1')
+    myW('OUTP OFF')
+    myW('OUTP:CH2 OFF')
+    myW('SYST:CLKSRC EXT')
+    myW('COUN ON')
+    myW('COUN:COUP DC')
+    print(myQ('COUN:COUP?'))
+    myW('COUN:SENS LOW')
+    print(myQ('COUN:SENS?'))
+    myW('COUN:TLEV 50')
+    print(myQ('COUN:TLEV?'))
+    myW('COUN:HFRS ON')
+    print(myQ('COUN:HFRS?'))
+    print(myQ('COUN:FREQ?'))
+
+if True:
+    myW('COUN ON')
 
 fname = 'dave_' + str(int(time.time())) + '.csv'
+
 with open(fname, "a") as ofh:
+    count = 0
     last_freq = 'boop'
     while True:
         freq = myQ('COUN:FREQ?')
